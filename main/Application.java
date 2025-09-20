3ff38d194ca0cf6da6886871e4fe29262176717f
jsfdevelopercom-sketch <jsf.developer.com@gmail.com>
2025-09-20 17:15:31 +0530

    Update Application.java

--- main/Application.java
+++ main/Application.java
@@ -2,7 +2,14 @@
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-@SpringBootApplication
+import org.springframework.boot.autoconfigure.data.r2dbc.R2dbcDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.r2dbc.R2dbcRepositoriesAutoConfiguration;
+import org.springframework.boot.autoconfigure.r2dbc.R2dbcAutoConfiguration;
+@SpringBootApplication(exclude = {
+        R2dbcAutoConfiguration.class,
+        R2dbcDataAutoConfiguration.class,
+        R2dbcRepositoriesAutoConfiguration.class
+})
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
